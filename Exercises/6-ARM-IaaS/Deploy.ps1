5999b6a864980011ad77854352a7f50fcdb7d5ae
Kunal Babre <kubabre@microsoft.com>
2017-08-24 08:37:49 +0100

    Work in Progress

---
+++ Exercises/6-ARM-IaaS/Deploy.ps1
@@ -0,0 +1,17 @@
+﻿#Login-AzureRmAccount -SubscriptionName "<SubscriptionName>"
+
+$rg="Demo-RG-PaaS"
+$loc="South Central US"
+$tf = ".\azuredeploy.json"
+$tpf =".\azuredeploy.parameters.json"
+$uDNS = $rg + "-TM"
+$uDNSWA = $rg + "-WA"
+
+#Create RG
+New-AzureRmResourceGroup -Name $rg -Location $loc
+
+#Validate
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $tf -TemplateParameterFile $tpf  -uniqueDnsName $uDNS -uniqueDnsNameForWebApp $uDNSWA -Verbose  
+
+#Deploy
+NEW-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $tf -TemplateParameterFile $tpf -uniqueDnsName $uDNS -uniqueDnsNameForWebApp $uDNSWA  -Verbose -Mode Incremental
---
+++ Exercises/6-ARM-IaaS/Final/Deploy.ps1
@@ -0,0 +1,25 @@
+﻿#Login-AzureRmAccount -SubscriptionName "<SubscriptionName>"
+
+Login-AzureRmAccount
+
+$rg="Demo-RG-IaaS2"
+$loc="South Central US"
+$tf = ".\azuredeploy.json"
+$tpf =".\azuredeploy.parameters.json"
+$vmCount = 2
+$dnsp = "kb123455-u"
+
+#Create RG
+New-AzureRmResourceGroup -Name $rg -Location $loc
+
+#Validate
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $tf -TemplateParameterFile $tpf -virtualMachineCount $vmCount -Verbose
+
+#Validate - debug errors
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $tf -TemplateParameterFile $tpf -virtualMachineCount $vmCount -Verbose -Debug
+
+#Fix and Validate
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $tf -TemplateParameterFile $tpf -virtualMachineCount $vmCount -dnsPrefixForPublicIP $dnsp -Verbose -Debug
+
+#Deploy
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $tf -TemplateParameterFile $tpf -virtualMachineCount $vmCount -dnsPrefixForPublicIP $dnsp -Verbose 
